f7bb223a8ab57bc64fc11ed3ba0b96c2758f9b46
Jatin Prakash <jatinprakash@pop-os.localdomain>
2020-10-09 10:59:19 +0530

    add gitignores

--- test_voc.py
+++ test_voc.py
@@ -7,9 +7,9 @@ from utils import colorize_mask, MaskToTensor
 
 # Create transforms
 input_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
 
 target_transform = MaskToTensor()
 
